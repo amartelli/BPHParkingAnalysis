509dcdc9eba2b0d4cde2d8b946da8f4987ec7f68
arabella <arabella.martelli@cern.ch>
2018-10-25 15:39:57 +0100

    fix absolute paths

--- NtupleProducer/scripts/config_data_runNtuples.sh
+++ NtupleProducer/scripts/config_data_runNtuples.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-BToKmumuNtupleProducer data BPHParking --input DUMMYINPUTFILELIST --output DUMMYOUTFILENAME --addNNBMX /vols/build/cms/amartell/BParking/SLX7/newOct16/CMSSW_10_2_0/src/BPHParkingAnalysis/NtupleProducer/data/weights_50.pb
+BToKmumuNtupleProducer data BPHParking --input DUMMYINPUTFILELIST --output DUMMYOUTFILENAME --addNNBMX BPHParkingAnalysis/NtupleProducer/data/weights_50.pb#REPLACE_WITH_ABSOLUTE_PATH
 
 
--- NtupleProducer/scripts/config_runAnalyis.sh
+++ NtupleProducer/scripts/config_runAnalyis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-./analyzeCharged_fastDATA --isEle 0 --dataset runA --run 1 --typeSelection tightCB --ntupleList DUMMYINPUTFILELIST --JOBid DUMMYJOBID --outputFolder /vols/cms/amartell/BParking/data_FinalNtuples/ 
+./analyzeCharged_fastDATA --isEle 0 --dataset runA --run 1 --typeSelection tightCB --ntupleList DUMMYINPUTFILELIST --JOBid DUMMYJOBID --outputFolder YOUR_OUTPUT_PATH
 
 
